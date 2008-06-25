63dcc79e70d183e2a8f0c370b1d68702ffe58831
René Mondeel <rene.mondeel@rivm.nl>
2008-06-25 16:12:21 +0000

    20080625 Weer terug.

---
+++ CZM/src/nl/rivm/emi/cdm/DomLevelTraverser.java
@@ -0,0 +1,31 @@
+package nl.rivm.emi.cdm;
+
+import java.util.TreeSet;
+
+import nl.rivm.emi.cdm.exceptions.CDMConfigurationException;
+import nl.rivm.emi.cdm.individual.Individual;
+
+import org.w3c.dom.Node;
+
+public abstract class DomLevelTraverser extends TreeSet<Individual>{
+ 
+	protected String myElementName = "Skblzz";
+	
+	protected Node traverseTreeLevel(Node node) throws CDMConfigurationException {
+		while (node != null) {
+			if (isMyElementName(node)) {
+//				Population population = new Population(node);
+//				setPopulation(population);
+			}
+			node = node.getNextSibling();
+		}
+		return node;
+	}
+
+	public boolean isMyElementName(Node node) {
+		return myElementName.equals(node.getNodeName());
+	}
+
+
+
+}
