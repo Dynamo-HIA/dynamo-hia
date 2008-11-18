60405d016a48290a37bde559c1101e0b9cd1b581
René Mondeel <rene.mondeel@rivm.nl>
2008-11-18 14:42:34 +0000

    20081117 Agnostic elaboration.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/exceptions/DynamoConfigurationException.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/exceptions/DynamoConfigurationException.java
@@ -1,6 +1,8 @@
 package nl.rivm.emi.dynamo.exceptions;
 
-public class DynamoConfigurationException extends Exception {
+import org.apache.commons.configuration.ConfigurationException;
+
+public class DynamoConfigurationException extends ConfigurationException {
 
 	public DynamoConfigurationException(String string) {super(string);
 		// TODO Auto-generated constructor stub
