a3e0f729a380034dea7ced7a1a830bf983548585
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2008-12-06 05:36:56 +0000

    20080512: running of different scenario's added + output factory

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/estimation/ScenarioInfo.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package nl.rivm.emi.dynamo.estimation;
+
+/**
+ * @author Hendriek
+ * This Class contains information on scenario's
+ */
+public class ScenarioInfo {
+
+	
+	int nScenarios=1; // nScenarios give the number of alternative scenario's (the baseline scenario not included)
+	boolean [] initialPrevalenceType ={false};
+	boolean[] transitionType ={false};
+	float[][][][] newPrevalence=null; /* indexes: scenario,age, sex, class */
+	float [][][] drift=null;    /* indexes: scenario,age, sex */
+	float [][][] stdDrift=null;    /* indexes: scenario,age, sex */
+	float [][][] offsetDrift=null; /*indexes: scenario,age, sex */
+	float succesrate = 0.5F;
+	float minAge = 20;
+	float maxAge = 50;
+	boolean inMen = true;
+	boolean inWomen = true;
+	
+	/**
+	 * 
+	 */
+	public ScenarioInfo() {
+		// TODO Auto-generated constructor stub
+	}
+	public void makeTestData (){
+		
+		initialPrevalenceType = new boolean[1];
+		initialPrevalenceType[0]=true;
+		
+		newPrevalence=new float [1][96][2][2];
+		
+		for (int a = 0; a < 96; a++) {
+			for (int g = 0; g < 2; g++) {
+				
+				newPrevalence[0][a][g][0]=0.5F;
+				newPrevalence[0][a][g][1]=0.5F;
+		;}
+
+}}
+	/**
+	 * @param scen
+	 * @return
+	 */
+	public float[][][][] getTransitionMatrix(int scen) {
+		// TODO Auto-generated method stub
+		return null;
+	}}
