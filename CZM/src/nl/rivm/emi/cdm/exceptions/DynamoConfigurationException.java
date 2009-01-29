e1d6e230faa5baa604269d5660f8a931f548783a
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-01-29 11:17:15 +0000

    20090121 some small changes made

---
+++ CZM/src/nl/rivm/emi/cdm/exceptions/DynamoConfigurationException.java
@@ -0,0 +1,11 @@
+package nl.rivm.emi.cdm.exceptions;
+
+import org.apache.commons.configuration.ConfigurationException;
+
+public class DynamoConfigurationException extends ConfigurationException {
+
+	public DynamoConfigurationException(String string) {super(string);
+		// TODO Auto-generated constructor stub
+	}
+
+}
