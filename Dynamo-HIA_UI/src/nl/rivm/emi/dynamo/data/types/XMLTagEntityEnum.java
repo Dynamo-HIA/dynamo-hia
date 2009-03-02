f4b328c97ca3479a4b72bd2c508f79b2c656858f
René Mondeel <rene.mondeel@rivm.nl>
2009-03-02 09:44:32 +0000

    20090302 Status quo.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/types/XMLTagEntityEnum.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/types/XMLTagEntityEnum.java
@@ -1,15 +1,17 @@
 package nl.rivm.emi.dynamo.data.types;
 
 import nl.rivm.emi.dynamo.data.types.atomic.Age;
+import nl.rivm.emi.dynamo.data.types.atomic.Alpha;
+import nl.rivm.emi.dynamo.data.types.atomic.Begin;
+import nl.rivm.emi.dynamo.data.types.atomic.Categories;
 import nl.rivm.emi.dynamo.data.types.atomic.Category;
-import nl.rivm.emi.dynamo.data.types.atomic.Classes;
 import nl.rivm.emi.dynamo.data.types.atomic.Duration;
-import nl.rivm.emi.dynamo.data.types.atomic.DurationClass;
+import nl.rivm.emi.dynamo.data.types.atomic.End;
 import nl.rivm.emi.dynamo.data.types.atomic.Name;
+import nl.rivm.emi.dynamo.data.types.atomic.UniqueName;
 import nl.rivm.emi.dynamo.data.types.atomic.Number;
 import nl.rivm.emi.dynamo.data.types.atomic.Percent;
 import nl.rivm.emi.dynamo.data.types.atomic.Probability;
-import nl.rivm.emi.dynamo.data.types.atomic.ReferenceClass;
 import nl.rivm.emi.dynamo.data.types.atomic.ReferenceValue;
 import nl.rivm.emi.dynamo.data.types.atomic.Sex;
 import nl.rivm.emi.dynamo.data.types.atomic.TransitionDestination;
@@ -23,21 +25,25 @@ public enum XMLTagEntityEnum {
 	SEX((XMLTagEntity)new Sex()),
 	NUMBER((XMLTagEntity)new Number()),
 	NAME((XMLTagEntity)new Name()),
+	UNIQUENAME((XMLTagEntity)new UniqueName()),
 	CATEGORY((XMLTagEntity)new Category()),
 	DURATION((XMLTagEntity)new Duration()),
 	TRANSITIONSOURCE((XMLTagEntity)new TransitionSource()),
 	TRANSITIONDESTINATION((XMLTagEntity)new TransitionDestination()),
 	PERCENTAGE((XMLTagEntity) new Percent()),
 	PROBABILITY((XMLTagEntity)new Probability()),
-	CLASSES((XMLTagEntity)new Classes()),
-	REFERENCECLASS((XMLTagEntity)new ReferenceClass()),
-	DURATIONCLASS((XMLTagEntity)new DurationClass()),
+	CLASSES((XMLTagEntity)new Categories()),
+//	REFERENCECLASS((XMLTagEntity)new ReferenceClass()),
+//	DURATIONCLASS((XMLTagEntity)new DurationClass()),
 	REFERENCEVALUE((XMLTagEntity)new ReferenceValue()),
-	STANDARDVALUE((XMLTagEntity)new Value());
+	STANDARDVALUE((XMLTagEntity)new Value()),
+	BEGIN((XMLTagEntity)new Begin()),
+	ALPHA((XMLTagEntity)new Alpha()),
+	END((XMLTagEntity)new End());
 	
 	private final XMLTagEntity theType;
 
-	private XMLTagEntityEnum(XMLTagEntity type) {
+	private XMLTagEntityEnum(XMLTagEntity type){
 		this.theType = type;
 	}
 
