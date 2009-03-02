1482866e1b77f2986869270513b44985b66bc61b
René Mondeel <rene.mondeel@rivm.nl>
2009-03-02 10:04:01 +0000

    20090302 Status quo.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/layers/StaxWriterEntryPoint.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/layers/StaxWriterEntryPoint.java
@@ -11,10 +11,15 @@ import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 
 import nl.rivm.emi.cdm.exceptions.UnexpectedFileStructureException;
-import nl.rivm.emi.dynamo.data.factories.FactoryEntryPoint;
+import nl.rivm.emi.dynamo.data.factories.XMLHandlingEntryPoint;
+<<<<<<< .mine
+import nl.rivm.emi.dynamo.data.interfaces.ICategoricalObject;
+import nl.rivm.emi.dynamo.data.interfaces.IReferenceClass;
+=======
+>>>>>>> .r294
 import nl.rivm.emi.dynamo.data.xml.structure.RootElementNamesEnum;
 
-public abstract class StaxWriterEntryPoint extends FactoryEntryPoint {
+public abstract class StaxWriterEntryPoint extends XMLHandlingEntryPoint {
 
 	protected StaxWriterEntryPoint(RootElementNamesEnum rootElement, boolean observable){
 		super(rootElement, observable);
@@ -22,17 +27,4 @@ public abstract class StaxWriterEntryPoint extends FactoryEntryPoint {
 
 	// read
 	// write
-	public void writeToFile(File outputFile) throws XMLStreamException,
-			UnexpectedFileStructureException, IOException {
-		XMLOutputFactory factory = XMLOutputFactory.newInstance();
-		Writer fileWriter;
-		fileWriter = new FileWriter(outputFile);
-		XMLEventWriter writer = factory.createXMLEventWriter(fileWriter);
-		XMLEventFactory eventFactory = XMLEventFactory.newInstance();
-		streamEvents(writer, eventFactory);
-		writer.flush();
-	}
-
-	public abstract void streamEvents(XMLEventWriter writer, XMLEventFactory eventFactory)
-			throws XMLStreamException;
 }
