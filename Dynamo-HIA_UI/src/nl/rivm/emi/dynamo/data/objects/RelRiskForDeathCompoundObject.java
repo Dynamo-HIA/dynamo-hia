00cb280d7182be45b7a2c5fabe242f77e4e2d0b2
René Mondeel <rene.mondeel@rivm.nl>
2009-03-02 09:40:25 +0000

    20090302 Status quo.

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RelRiskForDeathCompoundObject.java
@@ -0,0 +1,18 @@
+package nl.rivm.emi.dynamo.data.objects;
+
+import nl.rivm.emi.dynamo.data.TypedHashMap;
+import nl.rivm.emi.dynamo.data.types.XMLTagEntitySingleton;
+import nl.rivm.emi.dynamo.data.types.atomic.Age;
+
+public class RelRiskForDeathCompoundObject extends TypedHashMap<Age> implements StandardObjectMarker{
+	private static final long serialVersionUID = 2374485744776391715L;
+
+	/**
+	 * Initialize self and copy content.
+	 * @param manufacturedMap
+	 */
+		public RelRiskForDeathCompoundObject(TypedHashMap<Age> manufacturedMap) {
+			 super((Age)XMLTagEntitySingleton.getInstance().get(Age.getElementName()));
+			 putAll(manufacturedMap);
+		}
+}
