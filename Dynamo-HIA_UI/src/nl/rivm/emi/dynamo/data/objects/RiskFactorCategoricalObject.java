d2b1b539a53799f1313cd0990ed2e3867d0fb108
René Mondeel <rene.mondeel@rivm.nl>
2009-03-02 10:57:13 +0000

    20090302 Status quo.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorCategoricalObject.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorCategoricalObject.java
@@ -1,8 +1,8 @@
 package nl.rivm.emi.dynamo.data.objects;
-
 /**
  * Model Object for the configuration of a categorical riskfactor.
  */
+import java.io.File;
 import java.util.List;
 
 import javax.xml.stream.XMLEventFactory;
@@ -10,16 +10,16 @@ import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 
+import nl.rivm.emi.dynamo.data.factories.XMLHandlingEntryPoint;
 import nl.rivm.emi.dynamo.data.interfaces.ICategoricalObject;
-import nl.rivm.emi.dynamo.data.interfaces.IReferenceCategory;
+import nl.rivm.emi.dynamo.data.interfaces.IReferenceClass;
 import nl.rivm.emi.dynamo.data.interfaces.IStaxEventContributor;
 import nl.rivm.emi.dynamo.data.objects.layers.CategoricalObjectImplementation;
-import nl.rivm.emi.dynamo.data.objects.layers.ConfigurationObjectBase;
-import nl.rivm.emi.dynamo.data.objects.layers.ReferenceCategoryObjectImplementation;
+import nl.rivm.emi.dynamo.data.objects.layers.ReferenceClassObjectImplementation;
 import nl.rivm.emi.dynamo.data.objects.layers.StaxWriterEntryPoint;
 import nl.rivm.emi.dynamo.data.types.XMLTagEntitySingleton;
 import nl.rivm.emi.dynamo.data.types.atomic.XMLTagEntity;
-import nl.rivm.emi.dynamo.data.types.markers.IHandlerType;
+import nl.rivm.emi.dynamo.data.types.interfaces.IXMLHandlingLayer;
 import nl.rivm.emi.dynamo.data.xml.structure.RootElementNamesEnum;
 import nl.rivm.emi.dynamo.exceptions.DynamoInconsistentDataException;
 
@@ -29,12 +29,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 
-public class RiskFactorCategoricalObject extends StaxWriterEntryPoint implements
-		IStaxEventContributor, IReferenceCategory, ICategoricalObject {
+public class RiskFactorCategoricalObject extends XMLHandlingEntryPoint implements
+		/* IXMLHandlingLayer, */ IReferenceClass, ICategoricalObject {
 	Log log = LogFactory.getLog(this.getClass().getName());
 
 	CategoricalObjectImplementation categoricalObjectImplementation;
-	ReferenceCategoryObjectImplementation referenceCategoryObjectImplementation;
+	ReferenceClassObjectImplementation referenceCategoryObjectImplementation;
 
 	public RiskFactorCategoricalObject(boolean makeObservable) {
 		super(RootElementNamesEnum.RISKFACTOR_CATEGORICAL, makeObservable);
@@ -60,7 +60,8 @@ public class RiskFactorCategoricalObject extends StaxWriterEntryPoint implements
 	 * NB makeObservable is a don't care here!
 	 */
 	public Object putCategory(Integer index, String name) {
-		return categoricalObjectImplementation.putCategory(index, name);
+		return categoricalObjectImplementation.putCategory(index,
+				name);
 	}
 
 	public Integer getReferenceCategory() {
@@ -71,7 +72,7 @@ public class RiskFactorCategoricalObject extends StaxWriterEntryPoint implements
 		return referenceCategoryObjectImplementation
 				.getObservableReferenceCategory();
 	}
-
+	
 	public Object putReferenceCategory(Integer index) {
 		return referenceCategoryObjectImplementation
 				.putReferenceCategory(index);
@@ -87,53 +88,66 @@ public class RiskFactorCategoricalObject extends StaxWriterEntryPoint implements
 	 */
 	@Override
 	public RiskFactorCategoricalObject manufacture(String dataFilePath)
-			throws ConfigurationException, DynamoInconsistentDataException {
+	 throws ConfigurationException, DynamoInconsistentDataException {
 		this.log.debug("Starting manufacture.");
 		manufacture(this, dataFilePath);
-		return this;
-	}
-
+	 return this;
+	 }
 	protected ConfigurationObjectBase handleRootChildren(
 			ConfigurationObjectBase modelObject,
 			List<ConfigurationNode> rootChildren) throws ConfigurationException {
-		if (rootChildren != null) {
-			for (ConfigurationNode rootChild : rootChildren) {
-				String childName = rootChild.getName();
-				log.debug("Handle rootChild: " + childName);
-				XMLTagEntity entity = XMLTagEntitySingleton.getInstance().get(
-						childName);
-				if ((entity != null) && (entity instanceof IHandlerType)) {
-					modelObject = ((IHandlerType) entity).handle(modelObject,
-							rootChild);
-				} else {
-					throw new ConfigurationException(
-							"Unhandled rootChild element: " + childName);
-				}
+	if(rootChildren != null){
+		for (ConfigurationNode rootChild : rootChildren) {
+			String childName = rootChild.getName();
+			log.debug("Handle rootChild: " + childName);
+			XMLTagEntity entity = XMLTagEntitySingleton.getInstance().get(
+					childName);
+			if ((entity != null) && (entity instanceof IXMLHandlingLayer)) {
+				modelObject = ((IXMLHandlingLayer) entity).handle(modelObject,
+						rootChild);
+			} else {
+				throw new ConfigurationException("Unhandled rootChild element: " + childName);
 			}
-		} else {
-			categoricalObjectImplementation.manufactureDefault();
-			referenceCategoryObjectImplementation.manufactureDefault();
 		}
+	} else {
+		categoricalObjectImplementation.manufactureDefault();
+		referenceCategoryObjectImplementation.manufactureDefault();
+	}
 		return modelObject;
 	}
 
 	// write
-	@Override
-	public void streamEvents(XMLEventWriter writer, XMLEventFactory eventFactory)
+	public void streamEvents(String value, XMLEventWriter writer, XMLEventFactory eventFactory)
 			throws XMLStreamException {
 		XMLEvent event = eventFactory.createStartDocument();
 		writer.add(event);
-		event = eventFactory.createStartElement("", "", this.rootElement
+		event = eventFactory.createStartElement("", "", rootElement
 				.getNodeLabel());
 		writer.add(event);
 		categoricalObjectImplementation.streamEvents(writer, eventFactory);
 		referenceCategoryObjectImplementation
-				.streamEvents(writer, eventFactory);
+				.streamEvents(null, writer, eventFactory);
 		event = eventFactory.createEndElement("", "", rootElement
 				.getNodeLabel());
 		writer.add(event);
 		event = eventFactory.createEndDocument();
 		writer.add(event);
+	}
+
+	public Float handle(ConfigurationNode node) throws ConfigurationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setDefault() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void fillHandlers(boolean observable) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
