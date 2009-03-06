969fd66ea56dd9dcf673b8a13b3c90cb2420c5c3
René Mondeel <rene.mondeel@rivm.nl>
2009-03-06 09:34:38 +0000

    20090306 Some cleaning up.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/NumberAgeTreeMap.java
+++
@@ -1,9 +0,0 @@
-package nl.rivm.emi.dynamo.data;
-
-import java.util.TreeMap;
-
-abstract public class NumberAgeTreeMap extends TreeMap<Number, Object>{
-
-	abstract protected Object get(Number arg0);
-
-}
