bd6d2993bf6b3193d2e96d2e0149931b29fa437b
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-03-24 15:02:06 +0000

    2009-03-24 bug removed

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/output/Output_LifeExpTab.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/output/Output_LifeExpTab.java
@@ -3,24 +3,13 @@
  */
 package nl.rivm.emi.dynamo.output;
 
-import nl.rivm.emi.dynamo.exceptions.DynamoOutputException;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
-import org.jfree.chart.JFreeChart;
 import org.jfree.experimental.chart.swt.ChartComposite;
 
 /**
@@ -35,7 +24,11 @@ public class Output_LifeExpTab  {
 	private LifeExpectancyChartFactory factory;
 	private Composite plotComposite;
 
-	public Output_LifeExpTab(TabFolder tabfolder , DynamoOutputFactory output) {
+	/**makes the folder for the life expectancy plots
+	 * @param tabfolder
+	 * @param output
+	 */
+	public Output_LifeExpTab(final TabFolder tabfolder , final DynamoOutputFactory output) {
 	this.tabFolder=tabfolder;
 	this.output=output;
 	this.factory=new LifeExpectancyChartFactory(output);
@@ -45,23 +38,26 @@ public class Output_LifeExpTab  {
 	makeIt();
 	}
 	
+	/**
+	 * makes the tabfolder
+	 */
 	public void makeIt(){
 		/* put the default plot information in the object plotInfo */
-		this.plotComposite = new Composite(tabFolder, SWT.FILL);
+		this.plotComposite = new Composite(this.tabFolder, SWT.FILL);
 		// plotComposite.setBounds(10,10,720,600);
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 2;
 		
-		plotComposite.setLayout(gridLayout);
+		this.plotComposite.setLayout(gridLayout);
 		
 		
-		plotComposite.setLayout(gridLayout);
-		plotInfo=new ButtonStates();
-		plotInfo.currentScen = 0;
-		plotInfo.currentDisease = 1;
-		plotInfo.currentYear = 0;
+		this.plotComposite.setLayout(gridLayout);
+		this.plotInfo=new ButtonStates();
+		this.plotInfo.currentScen = 0;
+		this.plotInfo.currentDisease = 1;
+		this.plotInfo.currentYear = 0;
 
-		Composite controlComposite = new Composite(plotComposite, SWT.NONE);
+		Composite controlComposite = new Composite(this.plotComposite, SWT.NONE);
 		GridLayout gridLayoutControl = new GridLayout();
 		gridLayoutControl.numColumns = 1;
 		GridData controlData = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
@@ -70,27 +66,27 @@ public class Output_LifeExpTab  {
 		controlComposite.setLayoutData(controlData);
 		
 		final ChartComposite chartComposite = new ChartComposite(
-				plotComposite, SWT.NONE, output.makeLifeExpectancyPlot(1), true);
+				this.plotComposite, SWT.NONE, this.output.makeLifeExpectancyPlot(0), true);
 		
 		 GridData chartData = new GridData(GridData.VERTICAL_ALIGN_FILL
 					| GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL
 					| GridData.GRAB_VERTICAL);
 		
 		chartComposite.setLayoutData(chartData);
-		String[] items = new String[output.getNDiseases() + 2];
-		String[] names = output.getDiseaseNames();
+		String[] items = new String[this.output.getNDiseases() + 2];
+		String[] names = this.output.getDiseaseNames();
 		items[0] = "none";
 		items[1] = "all";
 		for (int i = 0; i < names.length; i++)
 			items[i + 2] = names[i];
 
 		
-		new DiseaseChoiceGroup(controlComposite, chartComposite, factory, plotInfo, items);
+		new DiseaseChoiceGroup(controlComposite, chartComposite, this.factory, this.plotInfo, items);
 		
 		
-		final int minA = output.getMinAgeInSimulation();
-		int length = output.getMaxAgeInSimulation()
-				- output.getMinAgeInSimulation() + 1;
+		final int minA = Math.max(0,this.output.getMinAgeInSimulation());
+		int length = this.output.getMaxAgeInSimulation()
+				- minA + 1;
 		String[] ageNames = new String[length];
 		if (minA == 0)
 			ageNames[0] = "at birth";
@@ -98,16 +94,19 @@ public class Output_LifeExpTab  {
 			ageNames[0] = ((Integer) minA).toString();
 		for (int i = 1; i < length; i++)
 			ageNames[i] = ((Integer) (minA + i)).toString();
-		new AgeChoiceGroup(controlComposite, chartComposite, factory, plotInfo, ageNames);
+		new AgeChoiceGroup(controlComposite, chartComposite, this.factory, this.plotInfo, ageNames);
 		// chartComposite4.setBounds(0, 0, 400, 500);
-		TabItem item4 = new TabItem(tabFolder, SWT.NONE);
+		TabItem item4 = new TabItem(this.tabFolder, SWT.NONE);
 		item4.setText("life expectancy plots");
-		item4.setControl(plotComposite);
+		item4.setControl(this.plotComposite);
        
 	}
+	/**
+	 * 
+	 */
 	public void redraw(){
-		Control[] subcomp= plotComposite.getChildren();
-		factory.drawChartAction(plotInfo, (ChartComposite) subcomp[1]);
-		plotComposite.redraw();
+		Control[] subcomp= this.plotComposite.getChildren();
+		this.factory.drawChartAction(this.plotInfo, (ChartComposite) subcomp[1]);
+		this.plotComposite.redraw();
 	}
 		}
