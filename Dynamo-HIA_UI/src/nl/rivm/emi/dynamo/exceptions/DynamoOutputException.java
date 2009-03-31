442fbff29969e4bece065575a039947bcb2d9293
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-03-31 08:10:57 +0000

    2009-03-26 some cleanup

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/exceptions/DynamoOutputException.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/exceptions/DynamoOutputException.java
@@ -1,24 +1,13 @@
+/**
+ * 
+ */
 package nl.rivm.emi.dynamo.exceptions;
 
 /**
- * 
- * Exception thrown in case of an error in DynamoOutput
- * 
- * @author schutb
+ * @author boshuizh
  *
  */
 public class DynamoOutputException extends Exception {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4663216515410602278L;
-
-	/**
-	 * @param message
-	 */
-	public DynamoOutputException(String message) {
-		super(message);
-	}
 	
+	public DynamoOutputException (String s) {super(s);}
 }
