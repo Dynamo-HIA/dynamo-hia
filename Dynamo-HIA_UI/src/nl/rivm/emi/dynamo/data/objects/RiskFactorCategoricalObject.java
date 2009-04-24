92a1326879ffddd1f89a926b2cf73d1d21dafbf0
René Mondeel <rene.mondeel@rivm.nl>
2009-04-24 11:00:21 +0000

    20090424 Riskfactor continuous 2 Beta.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorCategoricalObject.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorCategoricalObject.java
@@ -10,71 +10,90 @@ import nl.rivm.emi.dynamo.data.TypedHashMap;
 import nl.rivm.emi.dynamo.data.interfaces.ICategoricalObject;
 import nl.rivm.emi.dynamo.data.interfaces.IReferenceClass;
 import nl.rivm.emi.dynamo.data.types.XMLTagEntityEnum;
-import nl.rivm.emi.dynamo.data.types.atomic.Index;
+import nl.rivm.emi.dynamo.data.types.atomic.FlexDex;
 import nl.rivm.emi.dynamo.data.util.AtomicTypeObjectTuple;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 
-public class RiskFactorCategoricalObject extends GroupConfigurationObjectServiceLayer
-		implements ICategoricalObject, IReferenceClass {
-		Log log = LogFactory.getLog(this.getClass().getName());
+public class RiskFactorCategoricalObject extends
+		GroupConfigurationObjectServiceLayer implements ICategoricalObject,
+		IReferenceClass {
+	Log log = LogFactory.getLog(this.getClass().getName());
 
-		public RiskFactorCategoricalObject(LinkedHashMap<String, Object> content) {
-			super();
-			super.putAll(content);
-		}
-		public String getCategoryName(Integer index) {
-			TypedHashMap<Index> wrappedObject = (TypedHashMap<Index>) get(XMLTagEntityEnum.CLASSES
-					.getElementName());
-			Object categoryNameObject = (String) wrappedObject.get(index);
-			String categoryName = "Empty";
-			if (categoryNameObject instanceof WritableValue) {
-				categoryName = (String) ((WritableValue) categoryNameObject)
-						.doGetValue();
-			} else {
-				categoryName = (String) categoryNameObject;
-			}
-			return categoryName;
-		}
-
-		public WritableValue getObservableCategoryName(Integer index) {
-			TypedHashMap<Index> wrappedObject = (TypedHashMap<Index>) get(XMLTagEntityEnum.CLASSES
-					.getElementName());
-			ArrayList<AtomicTypeObjectTuple> categoryNameTupleList = (ArrayList<AtomicTypeObjectTuple>) wrappedObject.get(index);
-			Object categoryNameObject = categoryNameTupleList.get(0).getValue();
-			WritableValue writableCategoryName = null;
-			if (categoryNameObject instanceof WritableValue) {
-				writableCategoryName = (WritableValue) categoryNameObject;
-			}
-			return writableCategoryName;
-		}
-
-		public int getNumberOfCategories() {
-			return ((TypedHashMap<Index>) get(XMLTagEntityEnum.CLASSES
-					.getElementName())).size();
-		}
-
-		public Object putCategory(Integer index, String name) {
-			TypedHashMap<Index> wrappedObject = (TypedHashMap<Index>) get(XMLTagEntityEnum.CLASSES
-					.getElementName());
-			WritableValue newName = new WritableValue(name, name.getClass());
-			return wrappedObject.put(index, newName);
-		}
-
-		public Integer getReferenceClass() {
-			return getSingleRootChildIntegerValue(XMLTagEntityEnum.REFERENCECLASS
-					.getElementName());
-		}
-
-		public WritableValue getObservableReferenceClass() {
-			return getSingleRootChildWritableValue(XMLTagEntityEnum.REFERENCECLASS
-					.getElementName());
-		}
-
-		public Object putReferenceClass(Integer index) {
-			return putSingleRootChildIntegerValue(XMLTagEntityEnum.REFERENCECLASS
-					.getElementName(), index);
-		}
+	public RiskFactorCategoricalObject(LinkedHashMap<String, Object> content) {
+		super();
+		super.putAll(content);
 	}
+
+	public String getCategoryName(Integer index) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CLASSES
+				.getElementName());
+		Object categoryNameObject = (String) wrappedObject.get(index);
+		String categoryName = "Empty";
+		if (categoryNameObject instanceof WritableValue) {
+			categoryName = (String) ((WritableValue) categoryNameObject)
+					.doGetValue();
+		} else {
+			categoryName = (String) categoryNameObject;
+		}
+		return categoryName;
+	}
+
+	public WritableValue getObservableCategoryName(Integer index) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CLASSES
+				.getElementName());
+		ArrayList<AtomicTypeObjectTuple> categoryNameTupleList = (ArrayList<AtomicTypeObjectTuple>) wrappedObject
+				.get(index);
+		Object categoryNameObject = categoryNameTupleList.get(0).getValue();
+		WritableValue writableCategoryName = null;
+		if (categoryNameObject instanceof WritableValue) {
+			writableCategoryName = (WritableValue) categoryNameObject;
+		}
+		return writableCategoryName;
+	}
+
+	public int getNumberOfCategories() {
+		log.debug("getNumberOfClasses() about to return " + ((TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CLASSES
+				.getElementName())).size());
+		return ((TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CLASSES
+				.getElementName())).size();
+	}
+
+	public Object putCategory(Integer index, String name) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CLASSES
+				.getElementName());
+		ArrayList<AtomicTypeObjectTuple> categoryNameTupleList = (ArrayList<AtomicTypeObjectTuple>) wrappedObject
+		.get(index);
+		AtomicTypeObjectTuple currentCategoryTuple = categoryNameTupleList.get(0);
+		Object currentCategory = currentCategoryTuple.getValue();
+		if (currentCategory == null) {
+			log
+					.fatal("!!!!!!!!!!putCategory() may not be used to add categories!!!!!!!!!!!!");
+		}
+		// Assumption, always writable.
+		WritableValue newName = new WritableValue(name, name.getClass());
+		currentCategoryTuple.setValue(newName);
+		categoryNameTupleList.remove(0);
+		categoryNameTupleList.add(0, currentCategoryTuple);
+		Object displacedObject = wrappedObject.put(index, categoryNameTupleList);
+		
+		return displacedObject;
+	}
+
+	public Integer getReferenceClass() {
+		return getSingleRootChildIntegerValue(XMLTagEntityEnum.REFERENCECLASS
+				.getElementName());
+	}
+
+	public WritableValue getObservableReferenceClass() {
+		return getSingleRootChildWritableValue(XMLTagEntityEnum.REFERENCECLASS
+				.getElementName());
+	}
+
+	public Object putReferenceClass(Integer index) {
+		return putSingleRootChildIntegerValue(XMLTagEntityEnum.REFERENCECLASS
+				.getElementName(), index);
+	}
+}
--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorContinuousObject.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/objects/RiskFactorContinuousObject.java
@@ -3,17 +3,22 @@ package nl.rivm.emi.dynamo.data.objects;
 /**
  * The putters are designed to insert Observables only!!!!!!
  */
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import nl.rivm.emi.dynamo.data.TypedHashMap;
+import nl.rivm.emi.dynamo.data.interfaces.ICutoffs;
 import nl.rivm.emi.dynamo.data.interfaces.IReferenceValue;
 import nl.rivm.emi.dynamo.data.types.XMLTagEntityEnum;
+import nl.rivm.emi.dynamo.data.types.atomic.FlexDex;
+import nl.rivm.emi.dynamo.data.util.AtomicTypeObjectTuple;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 
 public class RiskFactorContinuousObject extends
-		GroupConfigurationObjectServiceLayer implements IReferenceValue {
+		GroupConfigurationObjectServiceLayer implements ICutoffs, IReferenceValue {
 	private static final long serialVersionUID = -3767037849437274799L;
 
 	Log log = LogFactory.getLog(this.getClass().getName());
@@ -36,5 +41,59 @@ public class RiskFactorContinuousObject extends
 	public Object putReferenceValue(Float value) {
 		return putSingleRootChildFloatValue(XMLTagEntityEnum.REFERENCEVALUE
 				.getElementName(), value);
+	}
+
+	public Float getCutoffValue(Integer index) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CUTOFFS
+				.getElementName());
+		Object cutoffValueObject = (String) wrappedObject.get(index);
+		Float cutoffValue = null;
+		if (cutoffValueObject instanceof WritableValue) {
+			cutoffValue = (Float) ((WritableValue) cutoffValueObject)
+					.doGetValue();
+		} else {
+			cutoffValue = (Float) cutoffValueObject;
+		}
+		return cutoffValue;
+	}
+
+	public int getNumberOfCutoffs() {
+		log.debug("getNumberOfCutoffs() about to return " + ((TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CUTOFFS
+				.getElementName())).size());
+		return ((TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CUTOFFS
+				.getElementName())).size();
+}
+
+	public WritableValue getObservableCutoffValue(Integer index) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CUTOFFS
+				.getElementName());
+		ArrayList<AtomicTypeObjectTuple> cutoffValuesTupleList = (ArrayList<AtomicTypeObjectTuple>) wrappedObject
+				.get(index);
+		Object cutoffValueObject = cutoffValuesTupleList.get(0).getValue();
+		WritableValue writableCutoffValue = null;
+		if (cutoffValueObject instanceof WritableValue) {
+			writableCutoffValue = (WritableValue) cutoffValueObject;
+		}
+		return writableCutoffValue;
+}
+
+	public Object putCutoff(Integer index, Float value) {
+		TypedHashMap<FlexDex> wrappedObject = (TypedHashMap<FlexDex>) get(XMLTagEntityEnum.CUTOFFS
+				.getElementName());
+		ArrayList<AtomicTypeObjectTuple> cutoffValueTupleList = (ArrayList<AtomicTypeObjectTuple>) wrappedObject
+		.get(index);
+		AtomicTypeObjectTuple currentCutoffValueTuple = cutoffValueTupleList.get(0);
+		Object currentCutoffValue = currentCutoffValueTuple.getValue();
+		if (currentCutoffValue == null) {
+			log
+					.fatal("!!!!!!!!!!putCutoff() may not be used to add cutoffs!!!!!!!!!!!!");
+		}
+		// Assumption, always writable.
+		WritableValue newValue = new WritableValue(value, value.getClass());
+		currentCutoffValueTuple.setValue(newValue);
+		cutoffValueTupleList.remove(0);
+		cutoffValueTupleList.add(0, currentCutoffValueTuple);
+		Object displacedObject = wrappedObject.put(index, cutoffValueTupleList);
+		return displacedObject;
 	}
 }
