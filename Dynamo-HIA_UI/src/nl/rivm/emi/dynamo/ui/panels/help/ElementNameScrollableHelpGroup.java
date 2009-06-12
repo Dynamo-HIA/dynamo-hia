47ec7586bc2358fe13206af88889e41149cff5b6
René Mondeel <rene.mondeel@rivm.nl>
2009-06-12 08:54:27 +0000

    20090612 Simplest dynamically working solution. No scrolling.

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/panels/help/ElementNameScrollableHelpGroup.java
@@ -0,0 +1,104 @@
+package nl.rivm.emi.dynamo.ui.panels.help;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+
+public class ElementNameScrollableHelpGroup {
+	Log log = LogFactory.getLog(this.getClass().getName());
+	Composite parent = null;
+	public Group theGroup = null;
+	Label label = null;
+	Point preferredSize = null;
+	String borderText;
+	int count = 0;
+
+	public ElementNameScrollableHelpGroup(Composite parent, String borderText,
+			String elementName) {
+		this.parent = parent;
+		this.borderText = borderText;
+		theGroup = new Group(parent, SWT.V_SCROLL);
+		theGroup.setText(borderText);
+		FillLayout fillLayout = new FillLayout();
+		theGroup.setLayout(fillLayout);
+		setHelpText(null);
+	}
+
+	private void setHelpText(String helpText) {
+		if (label != null) {
+			// label.dispose();
+			// theGroup.setLayout(null);
+			// theGroup.setLayout(new FillLayout());
+		} else {
+			label = new Label(theGroup, SWT.WRAP);
+		}
+		Rectangle clientArea = theGroup.getClientArea();
+		// log.debug("PrePack: " + clientArea);
+		// theGroup.pack(true);
+		log.debug("PostPack: " + clientArea);
+		Point clientAreaSize = new Point(clientArea.width, clientArea.height);
+		log.debug("ClientArea size after early pack of group: "
+				+ clientAreaSize);
+		Point groupSize = theGroup.getSize();
+		log.debug("Group size: " + groupSize);
+		Point groupComp = theGroup.computeSize(0, 0);
+		log.debug("Computed group size: " + groupComp);
+		preferredSize = label.toControl(groupComp);
+		log.debug("Preferred size from computed group size: " + preferredSize);
+		preferredSize = label.toControl(clientArea.width, clientArea.height);
+		log.debug("Preferred size from positive inverted clientArea: "
+				+ preferredSize);
+		// label.setSize(preferredSize);
+		// label.setSize(new Point(clientArea.width, clientArea.height));// if
+		// (helpText == null) {
+		label.setBounds(clientArea);
+		String labelOutput = "Clientarea: " + clientArea + helpText
+				+ " Count: " + count;
+		label.setText(labelOutput);
+		log.debug(labelOutput);
+		// } else {
+		// label.setText(helpText);
+		// log.debug(helpText);
+		// }
+		Point computedSize = label
+				.computeSize(preferredSize.x, preferredSize.y);
+		log.debug("Computed size after fill: " + computedSize);
+		groupComp = theGroup.computeSize(0, 0);
+		log.debug("Group size after fill: " + groupComp);
+		label.setBackground(new Color(null, 0xee, 0xee, 0x00));
+		label.update();
+		count++;
+	}
+
+	public Group getGroup() {
+		return theGroup;
+	}
+
+	public void setHelpText(int id) {
+		Rectangle clientArea = theGroup.getClientArea();
+		String updatedLabelOutput = "x: "
+				+ clientArea.x
+				+ " y: "
+				+ clientArea.y
+				+ " h: "
+				+ clientArea.height
+				+ " w: "
+				+ clientArea.width
+				+ " Append Bots: "
+				+ id
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak."
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak."
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak."
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak."
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak."
+				+ " kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak kwak.";
+		setHelpText(updatedLabelOutput);
+	}
+}
