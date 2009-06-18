d3c3cc6754179578a6e0e144a01badf277579775
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-06-18 11:08:02 +0000

    file with errors

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/support/RelRisksCollectionForDropdown.java
@@ -0,0 +1,219 @@
+package nl.rivm.emi.dynamo.ui.support;
+
+import java.util.HashMap;
+
+import java.util.AbstractCollection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.NoSuchElementException;
+
+import nl.rivm.emi.dynamo.data.objects.DynamoSimulationObject;
+import nl.rivm.emi.dynamo.data.objects.tabconfigs.TabRelativeRiskConfigurationData;
+import nl.rivm.emi.dynamo.data.objects.tabconfigs.TabRiskFactorConfigurationData;
+import nl.rivm.emi.dynamo.ui.panels.simulation.DiseaseSelectionGroup;
+import nl.rivm.emi.dynamo.ui.panels.util.DropDownPropertiesSet;
+import nl.rivm.emi.dynamo.ui.treecontrol.BaseNode;
+import nl.rivm.emi.dynamo.ui.treecontrol.structure.StandardTreeNodeLabelsEnum;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 
+ * 
+ * Represents a String Set of diseases that have already been chosen. Is used to
+ * compile a list of chosable diseases.
+ * 
+ * @author schutb
+ * 
+ * @param <String>
+ */
+public class RelRisksCollectionForDropdown<String> extends
+		LinkedHashSet<String> {
+
+	private Log log = LogFactory.getLog(this.getClass().getName());
+
+	private static RelRisksCollectionForDropdown thisObject = null;
+	HashMap<String, HashMap<String, Set<String>>> availlableRelRisks = new HashMap<String, HashMap<String, Set<String>>>();
+
+	private RelRisksCollectionForDropdown() {
+		super();
+	}
+
+	/**
+	 * Get the single instance. There can be only one.
+	 * 
+	 * @param selectedNode
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	static synchronized public RelRisksCollectionForDropdown getInstance(
+			DynamoSimulationObject dynamoSimulationObject, BaseNode selectedNode)
+			throws ConfigurationException {
+		if (thisObject == null) {
+			thisObject = new RelRisksCollectionForDropdown();
+		}
+		thisObject.refresh(dynamoSimulationObject, selectedNode);
+		return thisObject;
+	}
+
+	/**
+	 * This method fills the object with a list containing all relative risks
+	 * that still can be choosen
+	 * 
+	 * 
+	 * @param dynamoSimulationObject
+	 *            : the simulation data that contains the information on which
+	 *            diseases and riskfactors and relative risks have allready been
+	 *            choosen
+	 * @throws ConfigurationException
+	 * 
+	 */
+	public void refresh(DynamoSimulationObject dynamoSimulationObject,
+			BaseNode selectedNode) throws ConfigurationException {
+
+		Set<String> validFromValues = new LinkedHashSet<String>();
+		Set<String> validToValues = new LinkedHashSet<String>();
+		Map<String, TabRiskFactorConfigurationData> rfc = dynamoSimulationObject
+		.getRiskFactorConfigurations();
+		Set<String> riskfactors = rfc.keySet();
+		if (riskfactors != null)
+			validFromValues.addAll(riskfactors);
+		Set<String> diseases = dynamoSimulationObject
+				.getDiseaseConfigurations().keySet();
+		if (diseases != null)
+			validToValues.addAll(diseases);
+		validToValues.add((String) "Death");
+		validToValues.add((String) "Disability");
+		if (diseases != null)
+			validFromValues.addAll(diseases);
+		TreeAsDropdownLists treeLists = TreeAsDropdownLists
+				.getInstance(selectedNode);
+		/*
+		 * first get all possible relative risk based on the availlable XML
+		 * files in the selected Node location
+		 */
+
+		this.availlableRelRisks = treeLists.getValidRelRisk();
+
+		Set<String> fromList = (Set<String>) availlableRelRisks.keySet();
+
+		/*
+		 * remove all entries of diseases and riskfactors which have not been
+		 * selected in the current simulation
+		 */
+
+		Iterator<String> it = fromList.iterator();
+		while (it.hasNext()) {
+			if (!validFromValues.contains(it.next()))
+				remove(it);
+			else {
+				Iterator<String> it2 = iterator();
+				while (it2.hasNext()) {
+					if (!validToValues.contains(it2.next()))
+						remove(it2);
+
+				}
+
+			}
+		}
+		Map<Integer, TabRelativeRiskConfigurationData> selectedRelRisks = dynamoSimulationObject
+				.getRelativeRiskConfigurations();
+
+		removeSelected(selectedRelRisks, diseases);
+
+	}
+
+	/**
+	 * 
+	 * Removes the relative risks that are not possible due to selections of
+	 * relative risks
+	 * 
+	 * @param chosenDiseaseName
+	 */
+	public void removeSelected(
+			Map<Integer, TabRelativeRiskConfigurationData> selectedRelRisks,
+			Set<String> diseaseNames) {
+
+		/*
+		 * check first if the particular relative risk has already been selected
+		 * if yes, remove it
+		 */
+		{
+			Iterator<Integer> it1 = selectedRelRisks.keySet().iterator();
+			while (it1.hasNext()) {
+				TabRelativeRiskConfigurationData currentSelectedRR = selectedRelRisks
+						.get(it1.next());
+				Iterator<String> it2 = availlableRelRisks.keySet().iterator();
+				while (it2.hasNext()) {
+					String currentFrom = it2.next();
+					HashMap<String, Set<String>> currentRRForChoice = availlableRelRisks
+							.get(currentFrom);
+					if (currentFrom.equals(currentSelectedRR.getFrom())) {
+						Iterator<String> it3 = currentRRForChoice.keySet()
+								.iterator();
+						while (it3.hasNext()) {
+							if (it3.next().equals(currentSelectedRR.getTo()))
+								it3.remove();
+						}
+					}
+					if (currentRRForChoice.isEmpty())
+						it2.remove();
+				}
+
+			}
+		}
+
+		/*
+		 * remove diseases that have been choosen as from diseases from the list
+		 * of "to" diseases, as this is not allowed in Dynamo
+		 */
+
+		Iterator<Integer> it4 = selectedRelRisks.keySet().iterator();
+		while (it4.hasNext()) {
+			TabRelativeRiskConfigurationData currentSelectedRR = selectedRelRisks
+					.get(it4.next());
+			if (diseaseNames.contains(currentSelectedRR.getFrom())) {
+				Iterator<String> it5 = availlableRelRisks.keySet().iterator();
+				while (it5.hasNext()) {
+					/* the key in this map is the name of the to disease */
+					HashMap<String, Set<String>> currentRRToChoices = availlableRelRisks
+							.get(it5.next());
+					currentRRToChoices.remove(currentSelectedRR.getFrom());
+
+				}
+
+			}
+		}
+	};
+
+// TODO methods that return the dropdown set that are necessary 
+
+	/**
+	 * this method updates the list directly from the DynamoSimulationObject
+	 * author: hendriek
+	 * 
+	 * @param dynamoSimulationObject
+	 *            : the DynamoSimulationObject
+	 */
+	public void updateChosenToList(DynamoSimulationObject dynamoSimulationObject) {
+
+		Map<Integer, TabRelativeRiskConfigurationData> relRiskConfiguration = dynamoSimulationObject
+				.getRelativeRiskConfigurations();
+		TabRelativeRiskConfigurationData singleRRconfiguration;
+		this.clear();
+		for (Integer key : relRiskConfiguration.keySet())
+
+		{
+			singleRRconfiguration = relRiskConfiguration.get(key);
+			this.add((String) singleRRconfiguration.getTo());
+
+		}
+
+	}
+
+}
