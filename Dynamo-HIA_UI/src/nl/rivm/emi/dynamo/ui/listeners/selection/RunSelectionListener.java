0b872d687b41ed01ea8f5c182c13e45f47f691f1
René Mondeel <rene.mondeel@rivm.nl>
2009-07-06 09:14:43 +0000

    20090706 Configuration base-directory and simulationname are now passed correctly both for existing and new simulation-configurations.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/listeners/selection/RunSelectionListener.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/listeners/selection/RunSelectionListener.java
@@ -10,6 +10,7 @@ import nl.rivm.emi.dynamo.ui.main.SimulationModal;
 import nl.rivm.emi.dynamo.ui.treecontrol.BaseNode;
 import nl.rivm.emi.dynamo.ui.treecontrol.ChildNode;
 import nl.rivm.emi.dynamo.ui.treecontrol.RootNode;
+import nl.rivm.emi.dynamo.ui.treecontrol.structure.StandardTreeNodeLabelsEnum;
 
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -29,38 +30,57 @@ public class RunSelectionListener extends AbstractLoggingClass implements
 				+ " got widgetDefaultSelected callback.");
 	}
 
+	// Upto V1.09_20090701
+	// public void widgetSelected(SelectionEvent arg0) {
+	// log.info("Control " + ((Control) arg0.getSource()).getClass().getName()
+	// + " got widgetSelected callback.");
+	// String filePath = modalParent.getConfigurationFilePath();
+	// BaseNode selectedNode = ((SimulationModal)modalParent).getBaseNode();
+	// BaseNode currentNode = selectedNode;
+	// BaseNode parentNode = (BaseNode) ((ChildNode) currentNode).getParent();
+	//		
+	// String parentName = parentNode.deriveNodeLabel();
+	//				
+	// while (!(parentNode instanceof RootNode)) {
+	// currentNode = parentNode;
+	// parentNode = (BaseNode) ((ChildNode) currentNode).getParent();
+	// }
+	// String baseDirectoryPath = currentNode.getPhysicalStorage()
+	// .getAbsolutePath();
+	// @SuppressWarnings("unused")
+	// BaseDirectory baseDirectory =
+	// BaseDirectory.getInstance(baseDirectoryPath);
+	// String simulationName = parentName;
+	// File configurationFile = new File(filePath);
+	//		
+	// // Run the dynamo simulation model (the CDM)
+	// runDynamoSimulation(simulationName, baseDirectoryPath);
+	// }
+
 	public void widgetSelected(SelectionEvent arg0) {
 		log.info("Control " + ((Control) arg0.getSource()).getClass().getName()
 				+ " got widgetSelected callback.");
 		String filePath = modalParent.getConfigurationFilePath();
-		BaseNode selectedNode = ((SimulationModal)modalParent).getBaseNode();
-		BaseNode currentNode = selectedNode;
-		BaseNode parentNode = (BaseNode) ((ChildNode) currentNode).getParent();
-		
-		String parentName = parentNode.deriveNodeLabel();
-				
-		while (!(parentNode instanceof RootNode)) {
-			currentNode = parentNode;
-			parentNode = (BaseNode) ((ChildNode) currentNode).getParent();
-		}
-		String baseDirectoryPath = currentNode.getPhysicalStorage()
-				.getAbsolutePath();
-		@SuppressWarnings("unused")
-		BaseDirectory baseDirectory = BaseDirectory.getInstance(baseDirectoryPath);
-		String simulationName = parentName;
-		File configurationFile = new File(filePath);
-		
+		String simulationDirectory = filePath.substring(0, filePath
+				.lastIndexOf(File.separatorChar));
+		String simulationName = simulationDirectory.substring(
+				simulationDirectory.lastIndexOf(File.separatorChar)+1,
+				simulationDirectory.length());
+		String baseDirectoryPath = simulationDirectory.substring(0,
+				simulationDirectory.lastIndexOf(File.separator
+						+ StandardTreeNodeLabelsEnum.SIMULATIONS.getNodeLabel()
+						+ File.separator));
 		// Run the dynamo simulation model (the CDM)
-		runDynamoSimulation(simulationName, baseDirectoryPath);			
+		runDynamoSimulation(simulationName, baseDirectoryPath);
 	}
 
 	private void runDynamoSimulation(String simName, String baseDir) {
-		// Use the parentShell 
-		Shell parentShell = ((SimulationModal)modalParent).getParentShell();
+		// Use the parentShell
+		Shell parentShell = ((SimulationModal) modalParent).getParentShell();
 
-		DynamoSimulationRunnable theSimulation = 
-			new DynamoSimulationRunnable(parentShell, simName, baseDir);
-		theSimulation.run();					
+		DynamoSimulationRunnable theSimulation = new DynamoSimulationRunnable(
+				parentShell, simName, baseDir);
+		theSimulation.run();
 	}
 
 }
