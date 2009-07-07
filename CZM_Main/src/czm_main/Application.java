f44ade499c89aa543baabdf835e5db0b1ce57d02
René Mondeel <rene.mondeel@rivm.nl>
2009-07-07 11:14:26 +0000

    20090707 Commit after the facts for release 1.09_20090701.

--- CZM_Main/src/czm_main/Application.java
+++ CZM_Main/src/czm_main/Application.java
@@ -1,13 +1,8 @@
 package czm_main;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import nl.rivm.emi.dynamo.ui.main.BaseStorageTreeScreen;
+import nl.rivm.emi.dynamo.ui.startup.ApplicationWrapper;
+import nl.rivm.emi.dynamo.ui.startup.BaseDirectoryHandler;
+import nl.rivm.emi.dynamo.ui.startup.BaseStorageTreeScreen;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,21 +18,10 @@ import org.eclipse.ui.PlatformUI;
  */
 public class Application implements IApplication {
 
-	private static final String APPBASENAME = "DYNAMO-HIA";
-	private static final String RELEASE_TAG = "release 1.09 20090619";
-	private static final String TREE_BASE = "tree.base";
-	private static final String defaultDataPath = System
-			.getProperty("user.dir")
-			+ File.separator
-			+ ".."
-			+ File.separator
-			+ ".."
-			+ File.separator
-			+ APPBASENAME;
-	private static final String iniFilePath = System.getProperty("user.dir")
-			+ File.separator + "ini" + File.separator + "lastopened.ini";
+//	private static final String APPBASENAME = "DYNAMO-HIA";
+//	private static final String RELEASE_TAG = "release 1.09 20090701";
 	// Logger of this class
-	static Log log = LogFactory.getLog(Application.class);
+	Log log = LogFactory.getLog(this.getClass().getName());
 
 	/*
 	 * (non-Javadoc)
@@ -46,59 +30,33 @@ public class Application implements IApplication {
 	 * IApplicationContext)
 	 */
 	public Object start(IApplicationContext context) throws Exception {
-
-		Display display = new Display();
-		Shell shell = null;
-		String baseDirectoryPath = determineDataDirectory();
-		BaseStorageTreeScreen application = new BaseStorageTreeScreen(
-				baseDirectoryPath);
-		shell = application.open(display);
-		baseDirectoryPath = application.getBaseDirectoryPath();
-		// Run the thread
-		if (shell != null) {
-			shell.setText(APPBASENAME + " " + RELEASE_TAG);
-			shell.open();
-			while (!shell.isDisposed()) {
-				if (!display.readAndDispatch())
-					display.sleep();
-			}
-			display.dispose();
-		}
-
+// 20090701 Delegated functionality to get rid of code-duplication. 
+//		BaseStorageTreeScreen application = null;
+//		Display display = new Display();
+//		Shell shell = null;
+//		do{
+//			BaseDirectoryHandler baseDirectoryHandler = new BaseDirectoryHandler(
+//					display);
+//			String baseDirectoryPath = baseDirectoryHandler.provideBaseDirectory();
+//			application = new BaseStorageTreeScreen(
+//					baseDirectoryPath);
+//			shell = application.open(display);
+//			log.debug("test completed normally.");
+//			if (shell != null) {
+//				shell.setText(ApplicationStatics.APPBASENAME + " " + ApplicationStatics.RELEASE_TAG);
+//				shell.open();
+//				while (!shell.isDisposed()) {
+//					if (!display.readAndDispatch())
+//						display.sleep();
+//				}
+//			}
+//			} while(BaseStorageTreeScreen.RESTART.equals(application.getRestartMessage()));
+//			display.dispose();
+//		return IApplication.EXIT_OK;
+		ApplicationWrapper theWrapper = new ApplicationWrapper();
+		theWrapper.startApplication();
 		return IApplication.EXIT_OK;
-
-	}
-
-	private String determineDataDirectory() {
-		String baseDirectoryPath = defaultDataPath;
-		Properties iniProperties = new Properties();
-		try {
-			FileInputStream fis = new FileInputStream(iniFilePath);
-			iniProperties.load(fis);
-			String propertyDirectoryPath = iniProperties.getProperty(TREE_BASE);
-			if (propertyDirectoryPath != null) {
-				baseDirectoryPath = propertyDirectoryPath;
-			}
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
-		} finally {
-			log.fatal("Tree-Base-Directory: " + baseDirectoryPath);
-			return baseDirectoryPath;
 		}
-	}
-
-	private void storeDataDirectory(String baseDirectoryPath) {
-		try {
-			if (baseDirectoryPath != null) {
-				Properties iniProperties = new Properties();
-				iniProperties.put(TREE_BASE, baseDirectoryPath);
-				FileOutputStream fos = new FileOutputStream(iniFilePath);
-				iniProperties.store(fos, "No comment");
-			}
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
-		}
-	}
 
 	/*
 	 * (non-Javadoc)
