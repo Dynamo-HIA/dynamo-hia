0608e1491b8bc0ddcf6a3d4e0ca677751b9d89bf
René Mondeel <rene.mondeel@rivm.nl>
2009-10-01 09:11:51 +0000

    20091001 New label for new release.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
@@ -2,6 +2,6 @@ package nl.rivm.emi.dynamo.ui.startup;
 
 public class ApplicationStatics {
 	public static final String APPBASENAME = "DYNAMO-HIA";
-	public static final String RELEASE_TAG = "release 1.10 20090803";
+	public static final String RELEASE_TAG = "release 1.10RC1 20091001";
 	public static final String DEFAULTWORKDATADIRECTORY = "TestData";
 }
