9994b79a2282a0eaeb48a7cc65bc1601694bc0fb
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-10-05 12:32:58 +0000

    2009-10-05 bug lifeexpectancy plot fixed

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/output/AgeChoiceGroup.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/output/AgeChoiceGroup.java
@@ -16,71 +16,73 @@ import org.jfree.experimental.chart.swt.ChartComposite;
 
 /**
  * @author boshuizh
- *
+ * 
  */
 public class AgeChoiceGroup {
-	
+
 	Composite controlComposite;
 	ChartComposite chartComposite;
 	PlotDrawer plotDrawer;
 	ButtonStates plotInfo;
-	Group listgroup ;
+	Group listgroup;
 
-	
 	public AgeChoiceGroup(final Composite controlComposite,
-			final ChartComposite chartComposite, final PlotDrawer plotDrawer, final ButtonStates plotInfo,String[] names) {
-		this.chartComposite=chartComposite;
-		this.controlComposite=chartComposite;
-		this.plotDrawer=plotDrawer;
-		this.plotInfo=plotInfo;
-		
-	
-			this.listgroup = new Group(controlComposite, SWT.VERTICAL
-					| SWT.V_SCROLL);
-			this.listgroup.setText("at age:");
-			/* tooltip tekst is added to make it possible to select this widget for
-			 * disabling. Do not change this text
-			 */
-			this.listgroup.setToolTipText("choose age to plot");
-			this.listgroup.setEnabled(true);
-			GridLayout gridLayoutGroup = new GridLayout();
-			gridLayoutGroup.numColumns = 3;
-			this.listgroup.setLayout(gridLayoutGroup);
+			final ChartComposite chartComposite, final PlotDrawer plotDrawer,
+			final ButtonStates plotInfo, String[] names) {
+		this.chartComposite = chartComposite;
+		this.controlComposite = chartComposite;
+		this.plotDrawer = plotDrawer;
+		this.plotInfo = plotInfo;
 
-			final Combo combo = new Combo(this.listgroup, SWT.DROP_DOWN
-					| SWT.READ_ONLY);
+		this.listgroup = new Group(controlComposite, SWT.VERTICAL
+				| SWT.V_SCROLL);
+		this.listgroup.setText("at age:");
+		/*
+		 * tooltip tekst is added to make it possible to select this widget for
+		 * disabling. Do not change this text
+		 */
+		this.listgroup.setToolTipText("choose age to plot");
+		this.listgroup.setEnabled(true);
+		GridLayout gridLayoutGroup = new GridLayout();
+		gridLayoutGroup.numColumns = 3;
+		this.listgroup.setLayout(gridLayoutGroup);
 
-			
-			combo.setItems(names);
-			combo.select(0);
-			combo.setEnabled(true);
+		final Combo combo = new Combo(this.listgroup, SWT.DROP_DOWN
+				| SWT.READ_ONLY);
 
-			combo.addSelectionListener(new SelectionAdapter() {
-				public void widgetSelected(SelectionEvent e) {
-					Combo combo = (Combo) e.getSource();
-					String[] names= combo.getItems();
-					
-					plotInfo.currentAge = Integer.parseInt(names[combo.getSelectionIndex()]); 
-					
-					//if (plotInfo.Sullivan && !plotInfo.newborns) plotInfo.currentAge = names[combo.getSelectionIndex()].parseInteger(); +plotInfo.currentYear;
-					//else plotInfo.currentAge = combo.getSelectionIndex();
-					
-					plotDrawer.drawChartAction(plotInfo,chartComposite);
-					 
-				}
-			});
-		}
-		
-		
-	public void disable(){
+		combo.setItems(names);
+		combo.select(0);
+		combo.setEnabled(true);
+
+		combo.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				Combo combo = (Combo) e.getSource();
+				String[] names = combo.getItems();
+				if (names[combo.getSelectionIndex()]
+						.equalsIgnoreCase("at birth"))
+					plotInfo.currentAge = 0;
+				else
+					plotInfo.currentAge = Integer.parseInt(names[combo
+							.getSelectionIndex()]);
+
+				// if (plotInfo.Sullivan && !plotInfo.newborns)
+				// plotInfo.currentAge =
+				// names[combo.getSelectionIndex()].parseInteger();
+				// +plotInfo.currentYear;
+				// else plotInfo.currentAge = combo.getSelectionIndex();
+
+				plotDrawer.drawChartAction(plotInfo, chartComposite);
+
+			}
+		});
+	}
+
+	public void disable() {
 		this.listgroup.setEnabled(false);
 	}
-	
-	public void enable(){this.listgroup.setEnabled(true);}
 
-}		
+	public void enable() {
+		this.listgroup.setEnabled(true);
+	}
 
-			
-
-	
-
+}
