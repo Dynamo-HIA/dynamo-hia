b5a0bc32e1a46c835a305c0e223ed76819d7c659
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-11-04 10:40:32 +0000

    2009-11-04 name of release changed

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
@@ -1,7 +1,9 @@
 package nl.rivm.emi.dynamo.ui.startup;
 
+import java.io.File;
+
 public class ApplicationStatics {
 	public static final String APPBASENAME = "DYNAMO-HIA";
-	public static final String RELEASE_TAG = "release 1.10MonoRC1.52 20091009";
-	public static final String DEFAULTWORKDATADIRECTORY = "TestData";
+	public static final String RELEASE_TAG = "Beta release 1.10MonoRC1.52 20091102";
+	public static final String DEFAULTWORKDATADIRECTORY = "DYNAMO-HIA"+File.separator+"Netherlands";
 }
