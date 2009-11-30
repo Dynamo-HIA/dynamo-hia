70c2c0b7d6bece0e10be60cf2b52fa9c2cbebfcd
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-11-30 08:21:22 +0000

    2009-11-30 toegevoegd: csv writers voor populatie

---
+++ CZM/src/nl/rivm/emi/cdm/characteristic/values/CSVCharacteristicValueWriter.java
@@ -0,0 +1,67 @@
+package nl.rivm.emi.cdm.characteristic.values;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Element;
+
+public class CSVCharacteristicValueWriter {
+	static Log log = LogFactory
+			.getLog("nl.rivm.emi.cdm.characteristic.CharacteristicWriter");
+
+	public static String generateString(
+			IntCharacteristicValue characteristicValue, int stepNumber) {
+		String returnString = "";
+
+		if (stepNumber >= characteristicValue.getRijtje().length) {
+			log.fatal("Attemp to get a value (arraysize: "
+					+ characteristicValue.getRijtje().length
+					+ ") from a nonexistent location: " + stepNumber);
+			return returnString;
+		} else {
+			int value = characteristicValue.getValue(stepNumber);
+			returnString = ((Integer) value).toString();
+			return returnString;
+		}
+	}
+
+	public static String generateString(
+			FloatCharacteristicValue characteristicValue, int stepNumber) {
+		String returnString = "";
+		if (stepNumber >= characteristicValue.getRijtje().length) {
+			log.fatal("Attemp to get a value (arraysize: "
+					+ characteristicValue.getRijtje().length
+					+ ") from a nonexistent location: " + stepNumber);
+			return returnString;
+		} else {
+			float value = characteristicValue.getValue(stepNumber);
+			returnString = ((Float) value).toString();
+			return returnString;
+		}
+
+	}
+
+	public static String generateString(
+			CompoundCharacteristicValue characteristicValue, int stepNumber) {
+		String returnString = "";
+		if (stepNumber >= characteristicValue.getRijtje().length) {
+			log.fatal("Attemp to get a value (arraysize: "
+					+ characteristicValue.getRijtje().length
+					+ ") from a nonexistent location: " + stepNumber);
+			return returnString;
+		} else {
+			float[] value = characteristicValue.getUnwrappedValue(stepNumber);
+			returnString = "";
+			for (int i = 0; i < value.length; i++) {
+				if (i == 0)
+					returnString = ((Float) value[i]).toString();
+				else
+					returnString += ";" + ((Float) value[i]).toString();
+
+			}
+			return returnString;
+		}
+	}
+
+}
