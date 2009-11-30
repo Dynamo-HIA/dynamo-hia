0723bef8d11d13c9422837355d612f44e83a1729
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2009-11-30 08:21:31 +0000

    2009-11-30 toegevoegd: csv writers voor populatie

---
+++ CZM/src/nl/rivm/emi/cdm/individual/CSVIndividualWriter.java
@@ -0,0 +1,65 @@
+package nl.rivm.emi.cdm.individual;
+
+import java.util.Iterator;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import nl.rivm.emi.cdm.characteristic.values.CSVCharacteristicValueWriter;
+import nl.rivm.emi.cdm.characteristic.values.CharacteristicValueBase;
+import nl.rivm.emi.cdm.characteristic.values.DOMCharacteristicValueWriter;
+import nl.rivm.emi.cdm.characteristic.values.CompoundCharacteristicValue;
+import nl.rivm.emi.cdm.characteristic.values.FloatCharacteristicValue;
+import nl.rivm.emi.cdm.characteristic.values.IntCharacteristicValue;
+import nl.rivm.emi.cdm.individual.Individual.CharacteristicValueIterator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author boshuizh This class houses a method that generates a CSV String for
+ *         an individual in a particular step
+ * 
+ */
+public class CSVIndividualWriter {
+
+	Log log = LogFactory.getLog(getClass().getName());
+
+	public CSVIndividualWriter() {
+		super();
+	}
+
+	public static String generateCSVrecord(Individual individual, int stepNumber) {
+		String individualRecord = "";
+
+		String elementName = individual.getElementName();
+		individualRecord += elementName + ";";
+		/* elementname in Dynamo contains "scen"+ scenarioNumber */
+		String label = individual.getLabel();
+
+		if (label == null)
+			label = "";
+		individualRecord += label ;
+
+		Iterator<CharacteristicValueBase> iterator = individual.iterator();
+		while (iterator.hasNext()) {
+			individualRecord+=";";
+			CharacteristicValueBase charVal = iterator.next();
+			if (charVal instanceof IntCharacteristicValue) {
+				individualRecord += CSVCharacteristicValueWriter
+						.generateString((IntCharacteristicValue) charVal,
+								stepNumber);
+			} else {
+				if (charVal instanceof FloatCharacteristicValue) {
+					individualRecord += CSVCharacteristicValueWriter.generateString(
+							(FloatCharacteristicValue) charVal, stepNumber);
+				} else if (charVal instanceof CompoundCharacteristicValue) {
+
+					individualRecord += CSVCharacteristicValueWriter.generateString(
+							(CompoundCharacteristicValue) charVal, stepNumber);
+				}
+			}
+		}
+		return individualRecord;
+
+	}
+}
