da640fb2392719d73522ba035eccca38489a2482
Hendriek Boshuizen <hendriek.boshuizen@rivm.nl>
2010-01-04 08:44:21 +0000

    2010-01-04 some parameter xml-file-factory classes added for compound (alfa + end/begin disease)

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/types/root/Alphas4Parameters.java
@@ -0,0 +1,10 @@
+package nl.rivm.emi.dynamo.data.types.root;
+
+import nl.rivm.emi.dynamo.data.types.interfaces.RootElementType;
+
+public class Alphas4Parameters extends RootElementType {
+	static final protected String XMLElementName = "alphas4p";
+	public Alphas4Parameters() {
+		super(XMLElementName);
+	}
+}
---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/data/types/root/RelativeRisks_End.java
@@ -0,0 +1,10 @@
+package nl.rivm.emi.dynamo.data.types.root;
+
+import nl.rivm.emi.dynamo.data.types.interfaces.RootElementType;
+
+public class RelativeRisks_End extends RootElementType {
+	static final protected String XMLElementName = "relativerisks_end";
+	public RelativeRisks_End() {
+		super(XMLElementName);
+	}
+}
