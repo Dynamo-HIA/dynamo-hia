a79ac5ee7b1eaa8f587ae014df3610c3c39eb77e
René Mondeel <rene.mondeel@rivm.nl>
2010-02-17 16:03:14 +0000

    20100217 Handling of "No other Risk Source found" fixed.

---
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/exceptions/NoRiskSourceFoundException.java
@@ -0,0 +1,6 @@
+package nl.rivm.emi.dynamo.exceptions;
+
+public class NoRiskSourceFoundException extends RuntimeException {
+	private static final long serialVersionUID = 2026300594333578162L;
+
+}
