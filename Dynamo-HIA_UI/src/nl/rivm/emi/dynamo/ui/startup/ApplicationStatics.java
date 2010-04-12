50a1d7bb49b65f7a0237230f469fa3acbcb418b2
René Mondeel <rene.mondeel@rivm.nl>
2010-04-12 14:38:23 +0000

    20100412 Releasetag updated.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class ApplicationStatics {
 	public static final String APPBASENAME = "DYNAMO-HIA";
-	public static final String RELEASE_TAG = "Version 1.1, Release date: 201000407, build 1138.";
+	public static final String RELEASE_TAG = "Version 1.1, Release date: 201000412, build 1152.";
 	public static final String DEFAULTWORKDATADIRECTORY = "DYNAMO-HIA"+File.separator+"Netherlands";
 }
