fe4f9c9c79b547cd461e06d3ad491c09e1e5c401
René Mondeel <rene.mondeel@rivm.nl>
2010-04-26 08:46:49 +0000

    20100426 Title bar text updated for todays(aka the final) release.

--- Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
+++ Dynamo-HIA_UI/src/nl/rivm/emi/dynamo/ui/startup/ApplicationStatics.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class ApplicationStatics {
 	public static final String APPBASENAME = "DYNAMO-HIA";
-	public static final String RELEASE_TAG = "Version 1.1, Release date: 201000420, build 1170.";
+	public static final String RELEASE_TAG = "Version 1.1, Release date: 201000426, build 1175.";
 	public static final String DEFAULTWORKDATADIRECTORY = "DYNAMO-HIA"+File.separator+"Netherlands";
 }
