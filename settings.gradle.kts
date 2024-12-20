80e07952fd77a7393e27d5c84a1f53a7d7e5c6d7
Niels Drost <n.drost@esciencecenter.nl>
2024-12-20 17:26:35 +0100

    bump build plugin versions (#34)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,10 @@
 
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
    
     // Apply the settings fix for the goomp plugin https://github.com/diffplug/goomph/issues/203
-    id("com.diffplug.configuration-cache-for-platform-specific-build") version "3.44.0"
+    id("com.diffplug.configuration-cache-for-platform-specific-build") version "4.2.0"
 }
 
 rootProject.name = "dynamo-hia"
